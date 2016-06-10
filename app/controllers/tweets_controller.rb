0c0cc204ad926bb55c5898c724f1fd8a4daa005b
Joao Moura <jg.moura.almeida@gmail.com>
2016-06-10 12:42:48 -0500

    Adds update, edit, create, new

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,46 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to "/tweets/#{@tweet.id}"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to "/tweets"
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to "/tweets/#{@tweet.id}"
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  private
+    def tweet_params
+      params.permit(:content)
+    end
+end
