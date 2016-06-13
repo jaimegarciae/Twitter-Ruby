4245a2d79b0f6046f134666b526be26f57396f6e
Joao Moura <jg.moura.almeida@gmail.com>
2016-06-13 14:58:29 -0500

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/about'           => 'pages#about'
   get '/contact'         => 'pages#contact'
-  root 'pages#home'
+  root 'tweets#index'
   #Tweets routes
   resources :tweets
 end
