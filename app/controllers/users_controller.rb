7b0ae0363c5fd7a3ed53993d7ed43db9adb81a19
Joao Moura <jg.moura.almeida@gmail.com>
2016-06-15 12:05:21 -0500

    Finish adding followed/following functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
   def profiles
     @users = User.all
   end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user.id)
+  end
 end
